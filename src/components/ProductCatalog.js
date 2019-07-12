ffc955c263176c3ac48831c2d1f274a88e1d07b7
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-07-12 17:20:14 +0530

    Product Catalog component (not responsive)

---
+++ src/components/ProductCatalog.js
@@ -0,0 +1,60 @@
+import React from "react";
+import HeadingTitle from "./HeadingTitle";
+
+function ProductCatalog() {
+  return (
+    <div className="container m-auto mt-5">
+      <HeadingTitle title="products" />
+      <div className="row">
+        <Product size="col-6" src="/static/products/t-shirts.jpg" name="T-Shirt" />
+        <div className="col-6">
+          <div className="row">
+            <Product size="col-12" src="/static/products/notebooks.jpg" name="notebooks" />
+            <Product size="col-6" src="/static/products/mugs.jpg" name="mugs" />
+            <Product size="col-6" src="/static/products/stickers.jpg" name="Stickers" />
+          </div>
+        </div>
+
+        {/*  */}
+        <div className="col-6">
+          <div className="row">
+            <Product size="col-6" src="/static/products/caps.jpg" name="Caps" />
+            <Product size="col-6" src="/static/products/badges.jpg" name="Badges" />
+            <Product size="col-12" src="/static/products/boxers.jpg" name="Boxers" />
+          </div>
+        </div>
+        <div className="col-6">
+          <div className="row">
+            <Product size="" src="/static/products/dress.jpg" name="Dress" />
+          </div>
+        </div>
+
+        {/*  */}
+        <Product size="col-6" src="/static/products/vests.jpg" name="Tank Tops & Vests" />
+        <div className="col-6">
+          <div className="row">
+            <Product size="col-12" src="/static/products/notebooks.jpg" name="Notebooks" />
+            <Product size="col-6" src="/static/products/socks.jpg" name="Socks" />
+            <Product size="col-6" src="/static/products/monc.jpg" name="Mobile Covers" />
+          </div>
+        </div>
+        {/*  */}
+
+        <Product size="col-3" src="/static/products/pins.jpg" name="Pins" />
+        <Product size="col-3" src="/static/products/flipflopsBox.jpg" name="Flip Flops Box" />
+        <Product size="col-6" src="/static/products/backpacks.jpg" name="Bags" />
+      </div>
+    </div>
+  );
+}
+
+function Product({ src, name, size }) {
+  return (
+    <div className={`product position-relative ${size}`}>
+      <img src={src} alt={name} className="img-fluid " />
+      <h3 className="product__catalog__name">{name}</h3>
+    </div>
+  );
+}
+
+export default ProductCatalog;
